bac1984d33815c2a2b616cd1f3114a268122208e
jaredjj3 <jared@jaredjohnson.dev>
2020-04-27 11:40:34 -0400

    Simplify TodoListItem toggle and remove

--- TodoList.js
+++ TodoList.js
@@ -2,47 +2,17 @@ import React, { Component } from 'react';
 import TodoListItem from './TodoListItem';
 
 export default class TodoList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.onClick = this.onClick.bind(this);
-  }
-
-  /**
-   * Given a click event from a child component, read its
-   * data-todo-id, and call the appropriate action based
-   * on it.
-   */
-  onClick(event) {
-    const data = event.target.dataset;
-    if (!data.hasOwnProperty('todoId')) {
-      return;
-    }
-    if (!data.hasOwnProperty('todoAction')) {
-      return;
-    }
-    
-    const id = parseInt(data.todoId, 10);
-    if (Number.isNaN(id)) {
-      return;
-    }
-
-    switch(data.todoAction) {
-      case 'TOGGLE':
-        this.props.onTodoListItemToggle(id);
-        break;
-      case 'REMOVE':
-        this.props.onTodoListItemRemove(id);
-        break;
-      default:
-        console.error(`${data.todoAction} not handled`);
-    }
-  }
-
   render() {
     return (
-      <ul onClick={this.onClick}>
-        {this.props.todos.map((todo) => <TodoListItem key={todo.index} todo={todo} />)}
+      <ul>
+        {this.props.todos.map((todo) =>  (
+          <TodoListItem
+            key={todo.index}
+            todo={todo}
+            onToggle={this.props.onTodoListItemToggle}
+            onRemove={this.props.onTodoListItemRemove}
+          />
+        ))}
       </ul>
     );
   }
--- TodoListItem.js
+++ TodoListItem.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 
 export default class TodoListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onToggleClick = this.onToggleClick.bind(this);
+    this.onRemoveClick = this.onRemoveClick.bind(this);
+  }
+
+  onToggleClick(event) {
+    this.props.onToggle(this.props.todo.id);
+  }
+
+  onRemoveClick(event) {
+    this.props.onRemove(this.props.todo.id);
+  }
+
   render() {
     const todo = this.props.todo;
     const text = todo.isComplete ? `${todo.text} (completed)` : todo.text;
 
     return (
       <li>
-        <button 
-          data-todo-id={todo.id}
-          data-todo-action="TOGGLE"
-        >
+        <button onClick={this.onToggleClick}>
           toggle
         </button>
-        <button 
-          data-todo-id={todo.id}
-          data-todo-action="REMOVE"
-        >
+        <button onClick={this.onRemoveClick}>
           remove
         </button>
         <span>{text}</span>
